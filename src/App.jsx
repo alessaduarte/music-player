5504b317c04262454d01e0c4cd3bd6074fee534a
Alessa Duarte <alessaduartedesigner@gmail.com>
2025-10-26 17:27:36 -0300

    Updated Workflow

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import albumPlaceholder from './Assets/AlbumPlaceholder.png';
-import playMusicIcon from './Assets/PlayMusicIcon.svg';
 import searchMusicIcon from './Assets/SearchMusicIcon.svg';
 
 // Read credentials from environment variables (Create React App expects REACT_APP_ prefix)
@@ -9,7 +8,6 @@ const CLIENT_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID || '';
 const CLIENT_SECRET = process.env.REACT_APP_SPOTIFY_CLIENT_SECRET || '';
 
 const defaultCover = albumPlaceholder;
-const playIcon = playMusicIcon;
 const searchIcon = searchMusicIcon;
 
 export default function Main() {
